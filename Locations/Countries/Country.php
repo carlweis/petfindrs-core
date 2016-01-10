4681b9e4f90203878fea61de0ebd8af45b805f2f
Carl Weis <carl@carlweis.com>
2016-01-10 04:02:20 -0600

    Add country model

---
+++ Locations/Countries/Country.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\PetFinders\Locations\Countries;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Country extends Model
+{
+    protected $table = 'countries';
+
+    protected $fillable = []; // none are fillable
+
+}
