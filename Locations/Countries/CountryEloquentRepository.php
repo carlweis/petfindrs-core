ac39a1417734ef090afffee2ea80b9b87fe22fce
Carl Weis <carl@carlweis.com>
2016-01-10 06:09:56 -0600

    Add Country transformer

---
+++ Locations/Countries/CountryEloquentRepository.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\PetFinders\Locations\Countries;
+
+class CountryEloquentRepository implements CountryRepository
+{
+    /**
+     * Return all countries.
+     *
+     * @return mixed
+     */
+    public function all()
+    {
+        return Country::all();
+    }
+
+    /**
+     * Return all active countries.
+     *
+     * @return mixed
+     */
+    public function active()
+    {
+        return Country::where('active', 1)->get();
+    }
+
+    /**
+     * Return country by country code.
+     *
+     * @param $code
+     * @return mixed
+     */
+    public function findByCode($code)
+    {
+        return Country::where('code', $code)->firstOrFail();
+    }
+
+    /**
+     * Return country by name.
+     *
+     * @param $name
+     * @return mixed
+     */
+    public function findByName($name)
+    {
+        return Country::where('name', $name)->firstOrFail();
+    }
+
+    /**
+     * Return country by location.
+     *
+     * @param $latitude
+     * @param $longitude
+     * @return mixed
+     */
+    public function findByLocation($latitude, $longitude)
+    {
+        return Country::where([
+            'latitude' => $latitude,
+            'longitude' => $longitude
+        ])->firstOrFail();
+    }
+}
---
+++ Locations/Countries/CountryTransformer.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\PetFinders\Locations\Countries;
+
+use League\Fractal\TransformerAbstract;
+
+class CountryTransformer extends TransformerAbstract
+{
+    protected $availableEmbeds = [
+        'states'
+    ];
+
+    public function transform(Country $country)
+    {
+        return [
+            'id'         => (int)$country->id,
+            'code'       => $country->code,
+            'latitude'   => $country->latitude,
+            'longitude'  => $country->longitude,
+            'name'       => $country->name,
+            'slug'       => $country->slug,
+            'active'     => (boolean)$country->active,
+            'created_at' => (string)$country->created_at->toIso8601String(),
+            'updated_at' => (string)$country->updated_at->toIso8601String(),
+        ];
+    }
+
+    public function embedStates(Country $country)
+    {
+        $states = $country->states;
+        return $this->collection($states, new StateTransformer);
+    }
+}
